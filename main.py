27eac28f28a9c6cec2c7fafae4da470d8e8e622f
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-27 21:32:51 +0530

    MongoDB connection Satblished and Templates and static directory was set

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
 
-app = Flask("http://127.0.0.1:2000")
+app = FastAPI()
 
-@app.route('/')
-def index():
-    return "hattp"
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+db = MongoClient("mongodb://localhost:27017")
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    x = db.notes.notes.find({}) 
+    for i in x:
+        print(i)
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q:str|None = None):
+    return {"item_id": item_id, "q": q}
