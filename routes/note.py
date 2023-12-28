6a0e2e2be9bc1a4a42ffb7182e7e692ced5bb0e0
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-28 10:23:22 +0530

    organise cod into flders

--- routes/note.py
+++ routes/note.py
@@ -5,6 +5,7 @@ from config.db import conn
 from schemas.note import noteEntity, notesEntity 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from typing import Annotated
 
 
 note = APIRouter()
@@ -22,10 +23,19 @@ async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "notes":notes})
 
 
+@note.post("/")
+async def add_note(request: Request):
+    formdata = await request.form()
+    print(formdata)
+    # new_note = dict(formdata)
+    # new_note['important'] = True if new_note['important'] == 'on' else False
+    # print(new_note)
+    # x = conn.notes.notes.insert_one(new_note) 
+    x = conn.notes.notes.insert_one(dict(formdata)) 
+    return {"success": True, "id":x}
+
+
 # @note.get("/items/{item_id}")
 # def read_item(item_id: int, q:str|None = None):
 #     return {"item_id": item_id, "q": q}
 
-# @note.post("/")
-# def add_note(note: Note):
-#     return noteEntity(note)
